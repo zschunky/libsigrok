0308b5c4fe6126126c9d1fbb592584586f7ace50
Andreas Zschunke <andreasz77@gmx.net>
2016-03-05 21:55:02 +0100

    1st hantek 4032l version

--- src/hardware/hantek-4032l/api.c
+++ src/hardware/hantek-4032l/api.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the libsigrok project.
  *
- * Copyright (C) 2016 Andreas Zschunke <andreasz77@gmx.net>
+ * Copyright (C) 2016 Andreas Zschunke <andreas.zschunke@gmx.net>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,6 +20,107 @@
 #include <config.h>
 #include "protocol.h"
 
+// begin look up tables
+static const uint32_t s_h4032l_api_scan_options[] = {
+  SR_CONF_CONN,
+};
+
+static const uint32_t s_h4032l_api_driver_options[] = {
+  SR_CONF_LOGIC_ANALYZER, 
+};
+
+static const uint32_t s_h4032l_api_device_options[] = {
+  SR_CONF_SAMPLERATE | SR_CONF_LIST| SR_CONF_SET | SR_CONF_GET,
+  SR_CONF_CAPTURE_RATIO | SR_CONF_SET | SR_CONF_GET,
+  SR_CONF_LIMIT_SAMPLES | SR_CONF_SET | SR_CONF_GET,
+  SR_CONF_TRIGGER_MATCH | SR_CONF_LIST,
+	SR_CONF_CONN | SR_CONF_GET,
+};
+
+static const int32_t s_h4032l_api_triggers[] = {
+	SR_TRIGGER_ZERO,
+	SR_TRIGGER_ONE,
+	SR_TRIGGER_RISING,
+	SR_TRIGGER_FALLING,
+	SR_TRIGGER_EDGE,
+};
+
+static const uint64_t s_h4032l_api_sample_rates_sorted[] = {
+  SR_KHZ(1),
+  SR_KHZ(2),
+  SR_KHZ(4),
+  SR_KHZ(8),
+  SR_KHZ(16),
+  SR_HZ(31250),
+  SR_HZ(62500),
+  SR_KHZ(125),
+  SR_KHZ(250),
+  SR_KHZ(500),
+  SR_KHZ(625),
+  SR_HZ(781250),
+  SR_MHZ(1),
+  SR_KHZ(1250),
+  SR_HZ(1562500),
+  SR_MHZ(2),
+  SR_KHZ(2500),
+  SR_KHZ(3125),
+  SR_MHZ(4),
+  SR_MHZ(5),
+  SR_KHZ(6250),
+  SR_MHZ(10),
+  SR_KHZ(12500),
+  SR_MHZ(20),
+  SR_MHZ(25),
+  SR_MHZ(40),
+  SR_MHZ(50),
+  SR_MHZ(80),
+  SR_MHZ(100),
+  SR_MHZ(160),
+  SR_MHZ(200),
+  SR_MHZ(320),
+  SR_MHZ(400),
+};
+
+static const uint64_t s_h4032l_api_sample_rates_config[] = {
+  SR_MHZ(100),
+  SR_MHZ(50),
+  SR_MHZ(25),
+  SR_KHZ(12500),
+  SR_KHZ(6250),
+  SR_KHZ(3125),
+  SR_HZ(1562500),
+  SR_HZ(781250),
+  SR_MHZ(80),
+  SR_MHZ(40),
+  SR_MHZ(20),
+  SR_MHZ(10),
+  SR_MHZ(5),
+  SR_KHZ(2500),
+  SR_KHZ(1250),
+  SR_KHZ(625),
+  SR_MHZ(4),
+  SR_MHZ(2),
+  SR_MHZ(1),
+  SR_KHZ(500),
+  SR_KHZ(250),
+  SR_KHZ(125),
+  SR_HZ(62500),
+  SR_HZ(31250),
+  SR_KHZ(16),
+  SR_KHZ(8),
+  SR_KHZ(4),
+  SR_KHZ(2),
+  SR_KHZ(1),
+  0,
+  0,
+  0,
+  SR_MHZ(200),
+  SR_MHZ(160),
+  SR_MHZ(400),
+  SR_MHZ(320),
+};
+// end look up tables
+
 SR_PRIV struct sr_dev_driver hantek_4032l_driver_info;
 
 static int init(struct sr_dev_driver *di, struct sr_context *sr_ctx)
@@ -29,19 +130,86 @@ static int init(struct sr_dev_driver *di, struct sr_context *sr_ctx)
 
 static GSList *scan(struct sr_dev_driver *di, GSList *options)
 {
-	struct drv_context *drvc;
-	GSList *devices;
+  struct drv_context *context=di->context;
 
-	(void)options;
+  // scan for device selection option
+  while (options && ((struct sr_config *)options->data)->key != SR_CONF_CONN)
+    options=options->next;
 
-	devices = NULL;
-	drvc = di->context;
-	drvc->instances = NULL;
+  // get list of connected relevant usb devices
+  GSList *usb_device_list;
+  if (options)
+    usb_device_list=sr_usb_find(context->sr_ctx->libusb_ctx, g_variant_get_string(((struct sr_config *)options->data)->data, NULL));
+  else
+    usb_device_list=sr_usb_find(context->sr_ctx->libusb_ctx, H4032L_PROTOCOL_USB_DEVICE);
 
-	/* TODO: scan for devices, either based on a SR_CONF_CONN option
-	 * or on a USB scan. */
+  // assemlbe sr device list
+  GSList *device_list=NULL;
+  while (usb_device_list) {
+    // create device and set several properties
+    struct sr_dev_inst *device = g_malloc0(sizeof(struct sr_dev_inst));
+    device->driver = &hantek_4032l_driver_info;
+    device->status = SR_ST_INACTIVE;
+    device->inst_type = SR_INST_USB;
+    device->vendor = g_strdup("Hantek");
+    device->model = g_strdup("4032L");
+    device->conn=usb_device_list->data;
 
-	return devices;
+    // create channel groups
+    struct sr_channel_group *channel_groups[2];     
+    for (int group=0; group<2; group++) {
+      struct sr_channel_group *channel_group=g_malloc0(sizeof(struct sr_channel_group));
+      channel_group->name=g_strdup_printf("%c", 'A' + group);
+      channel_groups[group]=channel_group;
+      device->channel_groups = g_slist_append(device->channel_groups, channel_group);
+    }
+
+    // assemble channel list and add channel to channel groups
+    for (int index=0; index<32; index++) {
+      char channel_name[4];
+      sprintf(channel_name,"%c%d", 'A'+ (index&1), index /2);
+    
+      struct sr_channel *channel = sr_channel_new(device, index, SR_CHANNEL_LOGIC, TRUE, channel_name);
+      struct sr_channel_group *channel_group=channel_groups[index&1];
+      channel_group->channels = g_slist_append(channel_group->channels, channel);
+    }
+    
+    // create device context
+    struct h4032l_protocol_device_context *device_context = g_malloc0(sizeof(struct h4032l_protocol_device_context));
+
+    // initialize command packet
+    device_context->command_packet.magic=H4032L_PROTOCOL_COMMAND_PACKET_MAGIC;
+    device_context->command_packet.pwm_a=h4032l_protocol_voltage2pwm(1.5);
+    device_context->command_packet.pwm_b=h4032l_protocol_voltage2pwm(1.5);
+    device_context->command_packet.sample_size=16384;
+    device_context->command_packet.pre_trigger_size=1024;
+
+    // set status
+    device_context->status=H4032L_PROTOCOL_STATUS_IDLE;
+
+    // set ratio
+    device_context->gcapture_ratio=5;
+
+    // create libusb transfer 
+    device_context->usb_transfer=libusb_alloc_transfer(0);
+
+    // save device context
+    device->priv=device_context;
+
+    // append device
+    device_list = g_slist_append(device_list, device);
+      
+    // add device to known instances
+    context->instances = g_slist_append(context->instances, device);
+
+    // select next usb device
+    usb_device_list=usb_device_list->next;
+  }
+
+  // clean up
+  g_slist_free(usb_device_list);
+
+  return device_list;
 }
 
 static GSList *dev_list(const struct sr_dev_driver *di)
@@ -56,106 +224,225 @@ static int dev_clear(const struct sr_dev_driver *di)
 
 static int dev_open(struct sr_dev_inst *sdi)
 {
-	(void)sdi;
+  struct drv_context *context=sdi->driver->context;
+  sdi->status = SR_ST_ACTIVE;
 
-	/* TODO: get handle from sdi->conn and open it. */
-
-	sdi->status = SR_ST_ACTIVE;
-
-	return SR_OK;
+  return sr_usb_open(context->sr_ctx->libusb_ctx, sdi->conn);
 }
 
 static int dev_close(struct sr_dev_inst *sdi)
 {
-	(void)sdi;
+  sdi->status = SR_ST_INACTIVE;
+  //struct sr_usb_dev_inst *usb=sdi->conn;
+  //if (!usb->devhdl)
+    //return SR_ERR;
+  //libusb_release_interface(usb->devhdl, 0);
+  //libusb_close(usb->devhdl);
+  sr_usb_close(sdi->conn);
 
-	/* TODO: get handle from sdi->conn and close it. */
-
-	sdi->status = SR_ST_INACTIVE;
-
-	return SR_OK;
+  return SR_OK;
 }
 
 static int cleanup(const struct sr_dev_driver *di)
 {
-	dev_clear(di);
-
-	/* TODO: free other driver resources, if any. */
-
-	return SR_OK;
+  return std_dev_clear(di, NULL);
 }
 
 static int config_get(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
-	int ret;
+  (void)cg;
 
-	(void)sdi;
-	(void)data;
-	(void)cg;
+  struct h4032l_protocol_device_context *device_context=sdi->priv;
+  switch (key) {
+  case SR_CONF_SAMPLERATE:
+    *data = g_variant_new_uint64(s_h4032l_api_sample_rates_config[device_context->command_packet.sample_rate]);
+    return SR_OK;
+  case SR_CONF_CAPTURE_RATIO:
+    *data = g_variant_new_uint64(device_context->gcapture_ratio);
+    return SR_OK;
+  case SR_CONF_LIMIT_SAMPLES: 
+    *data = g_variant_new_uint64(device_context->command_packet.sample_size);
+    return SR_OK;
+	case SR_CONF_CONN: {
+      if (!sdi->conn)
+        return SR_ERR_ARG;
+      struct sr_usb_dev_inst *usb = sdi->conn;
+      char str[128];
+      snprintf(str, 128, "%d.%d", usb->bus, usb->address);
+      *data = g_variant_new_string(str);
+      return SR_OK;
+    }
+  }
 
-	ret = SR_OK;
-	switch (key) {
-	/* TODO */
-	default:
-		return SR_ERR_NA;
-	}
-
-	return ret;
+  return SR_OK;
 }
 
 static int config_set(uint32_t key, GVariant *data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
-	int ret;
+  (void)cg;
+  struct h4032l_protocol_device_context *device_context=sdi->priv;
+  struct h4032l_protocol_command_packet *command_packet=&device_context->command_packet;
+  switch (key) {
+    case SR_CONF_SAMPLERATE: {
+        uint64_t sample_rate=g_variant_get_uint64(data);
+        uint8_t i=0;
+        while (i < ARRAY_SIZE(s_h4032l_api_sample_rates_config) && s_h4032l_api_sample_rates_config[i] != sample_rate)
+          i++;
 
-	(void)data;
-	(void)cg;
+        if (i==ARRAY_SIZE(s_h4032l_api_sample_rates_config) || sample_rate == 0) {
+          sr_err("invalid sample rate");
+          return SR_ERR_SAMPLERATE;
+        }
+        command_packet->sample_rate=i;
 
-	if (sdi->status != SR_ST_ACTIVE)
-		return SR_ERR_DEV_CLOSED;
+        return SR_OK;
+      }
+    case SR_CONF_CAPTURE_RATIO: {
+		    uint64_t ratio = g_variant_get_uint64(data);
+        if (ratio > 100) {
+          sr_err("capture ratio should be between 0 ... 100");
+          return SR_ERR;
+        }
+        device_context->gcapture_ratio=ratio;
+        return SR_OK;
+      }
+    case SR_CONF_LIMIT_SAMPLES: {
+        uint64_t number_samples = g_variant_get_uint64(data);
+        number_samples+=511;
+        number_samples&=0xfffffe00;
+        if (number_samples < 2048 || number_samples > 64*1024*1024) {
+          sr_err("invalid sample range 2k...64M: %ld", number_samples);
+          return SR_ERR;
+        }
+        command_packet->sample_size=number_samples;
+        return SR_OK;
+      }
+  }
 
-	ret = SR_OK;
-	switch (key) {
-	/* TODO */
-	default:
-		ret = SR_ERR_NA;
-	}
-
-	return ret;
+  return SR_ERR_NA;
 }
 
 static int config_list(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
-	int ret;
+  (void)cg;
 
-	(void)sdi;
-	(void)data;
-	(void)cg;
+  GVariantBuilder var;
 
-	ret = SR_OK;
-	switch (key) {
-	/* TODO */
-	default:
-		return SR_ERR_NA;
-	}
+  switch (key) {
+    case SR_CONF_SCAN_OPTIONS: 
+      // connection options
+      *data = g_variant_new_fixed_array(G_VARIANT_TYPE_UINT32, s_h4032l_api_scan_options, ARRAY_SIZE(s_h4032l_api_scan_options), sizeof(uint32_t));
+      return SR_OK; 
+    case SR_CONF_DEVICE_OPTIONS:
+      if (sdi)
+        // device options
+        *data = g_variant_new_fixed_array(G_VARIANT_TYPE_UINT32, s_h4032l_api_device_options, ARRAY_SIZE(s_h4032l_api_device_options), sizeof(uint32_t));
+      else
+        // driver options
+        *data = g_variant_new_fixed_array(G_VARIANT_TYPE_UINT32, s_h4032l_api_driver_options, ARRAY_SIZE(s_h4032l_api_driver_options), sizeof(uint32_t));
+      return SR_OK; 
+    case SR_CONF_SAMPLERATE:
+      // list of supported samplerates
+      g_variant_builder_init(&var, G_VARIANT_TYPE("a{sv}"));
+      g_variant_builder_add(&var, "{sv}", "samplerates", g_variant_new_fixed_array(G_VARIANT_TYPE("t"), s_h4032l_api_sample_rates_sorted, ARRAY_SIZE(s_h4032l_api_sample_rates_sorted), sizeof(uint64_t)));
+      *data = g_variant_builder_end(&var);
+      return SR_OK; 
+    case SR_CONF_TRIGGER_MATCH:
+      *data = g_variant_new_fixed_array(G_VARIANT_TYPE_INT32, s_h4032l_api_triggers, ARRAY_SIZE(s_h4032l_api_triggers), sizeof(int32_t));
+      return SR_OK; 
+  }
 
-	return ret;
+  return SR_ERR_NA;
 }
 
 static int dev_acquisition_start(const struct sr_dev_inst *sdi, void *cb_data)
 {
-	(void)sdi;
 	(void)cb_data;
 
 	if (sdi->status != SR_ST_ACTIVE)
 		return SR_ERR_DEV_CLOSED;
 
-	/* TODO: configure hardware, reset acquisition state, set up
-	 * callbacks and send header packet. */
+  struct h4032l_protocol_device_context *device_context=sdi->priv;
+	struct sr_trigger *trigger=sr_session_trigger_get(sdi->session);
+  struct h4032l_protocol_command_packet *command_packet=&device_context->command_packet;
 
-	return SR_OK;
+  // calculate packet ratio
+  command_packet->pre_trigger_size=(command_packet->sample_size * device_context->gcapture_ratio)/100;
+
+  command_packet->trig_flags.enable_trigger1=0;
+  command_packet->trig_flags.enable_trigger2=0;
+  command_packet->trig_flags.trigger_and_logic=0;
+	if (trigger && trigger->stages) {
+    GSList *stages=trigger->stages;
+    struct sr_trigger_stage *stage1=stages->data;
+    if (stages->next) {
+      sr_err("only one trigger stage supported for now");
+      return SR_ERR;
+    }
+    command_packet->trig_flags.enable_trigger1=1;
+    command_packet->trigger[0].flags.edge_type=H4032L_PROTOCOL_TRIGGER_EDGE_TYPE_DISABLED;
+    command_packet->trigger[0].flags.data_range_enabled=0;
+    command_packet->trigger[0].flags.time_range_enabled=0;
+    command_packet->trigger[0].flags.combined_enabled=0;
+    command_packet->trigger[0].flags.data_range_type=H4032L_PROTOCOL_TRIGGER_DATA_RANGE_TYPE_MAX;
+    command_packet->trigger[0].data_range_mask=0;
+    command_packet->trigger[0].data_range_max=0;
+
+
+    GSList *channel=stage1->matches;
+    while (channel) {
+      struct sr_trigger_match *match=channel->data;
+
+      switch (match->match) {
+	      case SR_TRIGGER_ZERO:
+          command_packet->trigger[0].flags.data_range_enabled=1;
+          command_packet->trigger[0].data_range_mask|=(1<<match->channel->index);
+          break;
+	      case SR_TRIGGER_ONE:
+          command_packet->trigger[0].flags.data_range_enabled=1;
+          command_packet->trigger[0].data_range_mask|=(1<<match->channel->index);
+          command_packet->trigger[0].data_range_max|=(1<<match->channel->index);
+          break;
+	      case SR_TRIGGER_RISING:
+          if (command_packet->trigger[0].flags.edge_type!=H4032L_PROTOCOL_TRIGGER_EDGE_TYPE_DISABLED) {
+            sr_err("only one trigger signal with fall/rising/edge allowed");
+            return SR_ERR;
+          }
+          command_packet->trigger[0].flags.edge_type=H4032L_PROTOCOL_TRIGGER_EDGE_TYPE_RISE;
+          command_packet->trigger[0].flags.edge_signal=match->channel->index;
+          break;
+	      case SR_TRIGGER_FALLING:
+          if (command_packet->trigger[0].flags.edge_type!=H4032L_PROTOCOL_TRIGGER_EDGE_TYPE_DISABLED) {
+            sr_err("only one trigger signal with fall/rising/edge allowed");
+            return SR_ERR;
+          }
+          command_packet->trigger[0].flags.edge_type=H4032L_PROTOCOL_TRIGGER_EDGE_TYPE_FALL;
+          command_packet->trigger[0].flags.edge_signal=match->channel->index;
+          break;
+	      case SR_TRIGGER_EDGE:
+          if (command_packet->trigger[0].flags.edge_type!=H4032L_PROTOCOL_TRIGGER_EDGE_TYPE_DISABLED) {
+            sr_err("only one trigger signal with fall/rising/edge allowed");
+            return SR_ERR;
+          }
+          command_packet->trigger[0].flags.edge_type=H4032L_PROTOCOL_TRIGGER_EDGE_TYPE_TOGGLE;
+          command_packet->trigger[0].flags.edge_signal=match->channel->index;
+          break;
+        default:
+          sr_err("unknown trigger value");
+          return SR_ERR;
+      }
+
+      channel=channel->next;
+    }
+	}
+
+  usb_source_add(sdi->session, ((struct drv_context *)sdi->driver->context)->sr_ctx, 10000, h4032l_protocol_receive_data, sdi->driver->context);
+
+  // start capturing
+  return h4032l_protocol_start(sdi);
 }
 
 static int dev_acquisition_stop(struct sr_dev_inst *sdi, void *cb_data)
--- src/hardware/hantek-4032l/protocol.h
+++ src/hardware/hantek-4032l/protocol.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the libsigrok project.
  *
- * Copyright (C) 2016 Andreas Zschunke <andreasz77@gmx.net>
+ * Copyright (C) 2016 Andreas Zschunke <andreas.zschunke@gmx.net>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,19 +26,127 @@
 #include "libsigrok-internal.h"
 
 #define LOG_PREFIX "hantek-4032l"
+#define H4032L_PROTOCOL_USB_DEVICE "04b5.4032"
+#define H4032L_PROTOCOL_COMMAND_PACKET_MAGIC 0x017f
+#define H4032L_PROTOCOL_STATUS_PACKET_MAGIC 0x2B1A037F
+#define H4032L_PROTOCOL_START_PACKET_MAGIC 0x2B1A027F
+#define H4032L_PROTOCOL_END_PACKET_MAGIC 0x4D3C037F
 
-/** Private, per-device-instance driver context. */
-struct dev_context {
-	/* Model-specific information */
-
-	/* Acquisition settings */
-
-	/* Operational state */
-
-	/* Temporary state across callbacks */
-
+enum h4032l_protocol_trigger_edge_type {
+  H4032L_PROTOCOL_TRIGGER_EDGE_TYPE_RISE=0,
+  H4032L_PROTOCOL_TRIGGER_EDGE_TYPE_FALL,
+  H4032L_PROTOCOL_TRIGGER_EDGE_TYPE_TOGGLE,
+  H4032L_PROTOCOL_TRIGGER_EDGE_TYPE_DISABLED
 };
 
-SR_PRIV int hantek_4032l_receive_data(int fd, int revents, void *cb_data);
+enum h4032l_protocol_trigger_data_range_type {
+  H4032L_PROTOCOL_TRIGGER_DATA_RANGE_TYPE_MAX=0,
+  H4032L_PROTOCOL_TRIGGER_DATA_RANGE_TYPE_MIN_OR_MAX,
+  H4032L_PROTOCOL_TRIGGER_DATA_RANGE_TYPE_OUT_OF_RANGE,
+  H4032L_PROTOCOL_TRIGGER_DATA_RANGE_TYPE_WITHIN_RANGE
+};
+
+enum h4032l_protocol_trigger_time_range_type {
+  H4032L_PROTOCOL_TRIGGER_TIME_RANGE_TYPE_MAX=0,
+  H4032L_PROTOCOL_TRIGGER_TIME_RANGE_TYPE_MIN_OR_MAX,
+  H4032L_PROTOCOL_TRIGGER_TIME_RANGE_TYPE_OUT_OF_RANGE,
+  H4032L_PROTOCOL_TRIGGER_TIME_RANGE_TYPE_WITHIN_RANGE
+};
+
+enum h4032l_protocol_trigger_data_selection {
+  H4032L_PROTOCOL_TRIGGER_DATA_SELECTION_NEXT=0,
+  H4032L_PROTOCOL_TRIGGER_DATA_SELECTION_CURRENT,
+  H4032L_PROTOCOL_TRIGGER_DATA_SELECTION_PREV
+};
+
+enum h4032l_protocol_command {
+  H4032L_PROTOCOL_COMMAND_CONFIGURE=0x2b1a, // also arms the logic analyzer
+  H4032L_PROTOCOL_COMMAND_STATUS=0x4b3a,
+  H4032L_PROTOCOL_COMMAND_GET=0x6b5a
+};
+
+enum h4032l_protocol_status {
+  H4032L_PROTOCOL_STATUS_IDLE,
+  H4032L_PROTOCOL_STATUS_COMMAND_CONFIGURE,
+  H4032L_PROTOCOL_STATUS_COMMAND_STATUS,
+  H4032L_PROTOCOL_STATUS_RESPONSE_STATUS,
+  H4032L_PROTOCOL_STATUS_RESPONSE_STATUS_RETRY,
+  H4032L_PROTOCOL_STATUS_RESPONSE_STATUS_CONTINUE,
+  H4032L_PROTOCOL_STATUS_COMMAND_GET,
+  H4032L_PROTOCOL_STATUS_FIRST_TRANSFER,
+  H4032L_PROTOCOL_STATUS_TRANSFER,
+};
+
+struct __attribute__((__packed__)) h4032l_protocol_trigger {
+  struct {
+    uint32_t edge_signal:5;
+    uint32_t edge_type:2;
+    uint32_t :1;
+    uint32_t data_range_type:2;
+    uint32_t time_range_type:2;
+    uint32_t data_range_enabled:1;
+    uint32_t time_range_enabled:1;
+    uint32_t :2;
+    uint32_t data_sel:2;
+    uint32_t combined_enabled:1;
+  } flags;
+  uint32_t data_range_min;
+  uint32_t data_range_max;
+  uint32_t time_range_min;
+  uint32_t time_range_max;
+  uint32_t data_range_mask;
+  uint32_t combine_mask;
+  uint32_t combine_data;
+};
+
+
+struct __attribute__((__packed__)) h4032l_protocol_command_packet {
+  uint16_t magic; // 0x017f
+  uint8_t sample_rate;
+  struct {
+    uint8_t enable_trigger1:1;
+    uint8_t enable_trigger2:1;
+    uint8_t trigger_and_logic:1;
+  } trig_flags;
+  uint16_t pwm_a;  /*
+   word PwmA - channel A Vref PWM value, pseudocode:
+     -6V < ThresholdVoltage < +6V
+     Vref = 1.8-ThresholdVoltage
+     if Vref>10.0
+       Vref = 10.0
+     if Vref<-5.0
+       Vref = -5.0
+     pwm = ToInt((Vref + 5.0) / 15.0 * 4096.0)
+       if pwm>4095
+       pwm = 4095*/
+  uint16_t pwm_b;
+  uint16_t reserved;
+  uint32_t sample_size; // sample depth in bits per channel, 2k-64M, must be multiple of 512
+  uint32_t pre_trigger_size; // pretrigger buffer depth in bits, must be < sample_size
+  struct h4032l_protocol_trigger trigger[2];
+  uint16_t command;
+};
+
+struct __attribute__((__packed__)) h4032l_protocol_status_packet {
+  uint32_t magic;
+  uint32_t values;
+  uint32_t status;
+};
+
+/** Private, per-device-instance driver context. */
+struct h4032l_protocol_device_context {
+  enum h4032l_protocol_status status;
+  uint32_t remaining_samples;
+  struct h4032l_protocol_command_packet command_packet;
+  struct libusb_transfer *usb_transfer;
+  unsigned char buffer[512];
+  uint64_t gcapture_ratio;
+};
+
+
+SR_PRIV int h4032l_protocol_receive_data(int fd, int revents, void *cb_data);
+SR_PRIV uint16_t h4032l_protocol_voltage2pwm(double voltage);
+SR_PRIV void LIBUSB_CALL h4032l_protocol_usb_callback(struct libusb_transfer *transfer);
+SR_PRIV int h4032l_protocol_start(const struct sr_dev_inst *sdi);
 
 #endif
